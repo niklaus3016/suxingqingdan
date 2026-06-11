69e19ed35039575a7ea48b156c05bbf5092b896e
深圳丰佰瑞网络科技有限公司 <Jp182025@163.com>
2026-06-11 14:42:11 +0000

    Update config files to match qitaapp format

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.suxingqingdan.app',
+  appName: '速行清单',
+  webDir: 'dist',
+  android: {}
+};
+
+export default config;
